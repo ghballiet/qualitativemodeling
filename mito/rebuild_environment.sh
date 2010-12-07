f18d0031be50e088ff193937f5d542602bbea22c
glenrhunt@gmail.com <glenrhunt@gmail.com@ba280476-eb0c-b0e8-772d-e093667840c9>
2010-12-07 18:21:32 +0000

    updated rebuild script

--- mito/rebuild_environment.sh
+++ mito/rebuild_environment.sh
@@ -7,3 +7,11 @@ echo "Building Lisp bootstrap file..."
 cd includes/lisp
 sbcl --load bootstrap.lisp
 echo "Done."
+
+echo "Reloading JSON..."
+./encode_struct.sh places
+./encode_struct.sh quantities
+./encode_struct.sh claims
+./encode_struct.sh facts
+./encode_struct.sh predictions
+./encode_struct.sh beliefs > /dev/null
